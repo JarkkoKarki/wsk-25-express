964ad2dca2d0f29aae4b41450ab734ec9e09085f
JarkkoKarki <karkijarkko@gmail.com>
2025-04-01 13:08:40 +0300

    Feature: Add frontpage HTML and CSS with static serving

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import api from './api/index.js';
+import path from 'path';
 const app = express();
 
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my REST API');
+  const filePath = path.join(path.resolve(), 'src', 'frontpage', 'hello.html'); // Construct absolute path
+  res.sendFile(filePath);
 });
+
+app.use(
+  '/frontpage',
+  express.static(path.join(path.resolve(), 'src', 'frontpage'))
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
